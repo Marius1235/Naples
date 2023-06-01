dcd4729efeb7a84c2afd450c2884650da596a6c3
Sigurd <89576086+sigurdlm@users.noreply.github.com>
2023-06-01 14:20:35 +0200

    Added comments and small changes
    Removed test-names.

--- dbAPI/connectDB.js
+++ dbAPI/connectDB.js
@@ -1,5 +1,8 @@
 const sql = require('mssql');
 
+//Config is where we have the connection requirements for the DB
+//We are looking into putting the config in a new local file and importing it here for security reasons
+//    ->>>>>>>>>>>>>>>>>  Will put this as a task in the next sprint.
 const config = {
     server: 'munchi-serv.database.windows.net',
     database: 'munch-db',
@@ -11,9 +14,12 @@ const config = {
     },
 };
 
+//The pool variable is using the sql.ConnectionPool class with the config variable as a parameter to connect to the DB
 const pool = new sql.ConnectionPool(config);
 const poolConnect = pool.connect();
+console.log("Connected to DB");
 
+//We export the sql, poolConnect and pool variables so we can use them in index.js
 module.exports = {
     sql, 
     poolConnect,
--- dbAPI/index.js
+++ dbAPI/index.js
@@ -1,3 +1,4 @@
+// Imported files and dependencies
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -5,49 +6,57 @@ const { sql, poolConnect, pool } = require('./connectDB');
 const { MAX } = require('mssql');
 const router = express.Router();
 
+const PORT = process.env.PORT || 3002; //Setting the port for the api
 
+// Middleware for the api to use json and the router we have created in the top of the file
 app.use(express.json());
 app.use(router);
 app.use(cors());
 
-const PORT = process.env.PORT || 3002;
-
-app.get('/', function (req, res) {
+//Setting up a basic html page
+app.get('/', function (req, res) { 
     res.write("<!DOCTYPE html>");
     res.write("<html style='font-familt: Roboto, Arial, sans-serif;'>");
-    res.write("<head><title>Send it 69</title></head>");
-    res.write("<body><p>Send it 420</p></body>");
+    res.write("<head><title>Rest API</title></head>");
+    res.write("<body><p>Basic html page for the API</p></body>");
     res.write("</html>");
     res.end();
 });
-router.get('/test', async (req, res) => {
+
+//This is the get function where the api does a select statement on the database
+//and returns the result as a json object
+router.get('/MunchifiedPicture', async (req, res) => { 
     try {
         await poolConnect;
-        const result = await pool.request().query('SELECT * FROM MunchifiedPicture');
+        let result = await pool.request().query('SELECT * FROM MunchifiedPicture');
         res.json(result.recordset);
     } catch (err) {
         console.error("Error: ", err);
         res.status(500).send("Server error");
     }
-
 });
 
-
-router.post('/test', async (req, res) => {
+//This is the post function where the api does a query on the database
+//We insert the picture as a base64 string and then convert it to a buffer for validation
+//The database will receive the picture as hex
+router.post('/MunchifiedPicture', async (req, res) => {
     try {
         await poolConnect;
-        const {picture} = req.body;
-        const result = await pool
+        let {picture} = req.body;
+        let result = await pool
         .request()
         .input('picture', sql.VarBinary(MAX), new Buffer.from(picture, 'base64'))
-        .query(`INSERT INTO MunchifiedPicture (PictureBlob, PictureName, DateTime, BoothId) VALUES (@picture, 'test', getdate(), 10);`);
+        .query(`INSERT INTO MunchifiedPicture 
+        (PictureBlob, PictureName, DateTime, BoothId) 
+        VALUES (@picture, 'testName', getdate(), 10);`);
         res.status(200).json(result.recordset);
     } catch (err) {
         console.error("Error: ", err);
-        res.status(500).json({message: "No success"});
-        
+        res.status(500).json({message: "Unsuccessful POST."});
     } finally {
         sql.close();
     }
 });
+
+//App.listen is used to connect to the server on port 3002 the port declared in the top of the file
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
