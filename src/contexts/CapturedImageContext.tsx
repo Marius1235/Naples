0be0bdd15e9d55edc763fc1286498214a721748f
BjarkiNord <17627918+BjarkiNord@users.noreply.github.com>
2023-06-01 15:49:34 +0200

    Update CapturedImageContext.tsx

--- src/contexts/CapturedImageContext.tsx
+++ src/contexts/CapturedImageContext.tsx
@@ -14,7 +14,6 @@ type Props = {
 };
 
 // Context makes it easier to share data between pages. Without context you would have to reload the page to display the image.
-
 const CapturedImageProvider: FC<Props> = ({ children }) => {
   const [capturedImage, setCapturedImage] = useState<string | null>(null);
 
