0e08eb4583ae2fdf5094852eef29ff6691870080
BjarkiNord <17627918+BjarkiNord@users.noreply.github.com>
2023-06-06 11:03:22 +0200

    Update PreviewResultsPageComponent.tsx

--- src/components/PreviewResultsPageComponent.tsx
+++ src/components/PreviewResultsPageComponent.tsx
@@ -1,18 +1,34 @@
 // Imports:
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useContext } from "react";
+import { useContext, useRef, useState } from "react";
 import { CapturedImageContext } from "../contexts/CapturedImageContext";
-
-
+import { Link } from "react-router-dom";
 
 // Component for ActionPage with the name of the art and the image 
 // object
 const PreviewResultsPageComponent = () => {
+    
+	let imgRef = useRef<HTMLImageElement>(null);
+	const [imgIsResized, setImgIsResized] = useState(false);
+	
+	// Function to enlarge image
+	const enlargePicture = () => {
+		let img = imgRef.current as HTMLImageElement;
+		if(!imgIsResized) { // If image is not enlarged already scale it up.
+			// Set image size to 1.5 times original
+			img.style.transform = "scale(1.5)";
+			// Animation effect
+			img.style.transition = "transform 0.25s ease";
+			setImgIsResized(true);
+		}
+		else // Reset image size to original
+		{
+			img.style.transform = "scale(1)";
+			img.style.transition = "transform 0.25s ease";
+			setImgIsResized(false);
+		}
+	};
 
-    const enlargePicture = () => {
-        // Enlarge picture
-
-    };
     // Image from context, after it is munchified
     const capturedImage = useContext(CapturedImageContext);
     return (
@@ -21,11 +37,15 @@ const PreviewResultsPageComponent = () => {
                 <div className="row">
                     <div className="col-sm">
                         {/* Result 1 */}
-                        {/* {capturedImage?.capturedImage && (
-								<img src={capturedImage.capturedImage} alt="Munchified Image" />
-						)} */}
-                        <img src={require(`../assets/images/skriket.jpg`)} alt="Skrik by Edvard Munch" />
-                        Results 1
+                        {capturedImage?.capturedImage && (
+								<img src={capturedImage.capturedImage} ref={imgRef} onClick={enlargePicture} alt="Munchified Image" />
+						)}
+						{/* Renders the button if the image has been resized. */}
+                        {imgIsResized && (
+						<Link to="/ActionPage">
+							<button>Go to ActionPage</button>
+						</Link>
+					)}
                     </div>
                 </div>
             </div>
