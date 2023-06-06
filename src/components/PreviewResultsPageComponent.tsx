962a0dc4c31ad37d4c2fa44bce092fd8a1b20142
BjarkiNord <17627918+BjarkiNord@users.noreply.github.com>
2023-06-06 09:50:41 +0200

    Update PreviewResultsPageComponent.tsx

--- src/components/PreviewResultsPageComponent.tsx
+++ src/components/PreviewResultsPageComponent.tsx
@@ -4,24 +4,28 @@ import { useContext } from "react";
 import { CapturedImageContext } from "../contexts/CapturedImageContext";
 
 
+
 // Component for ActionPage with the name of the art and the image 
 // object
 const PreviewResultsPageComponent = () => {
 
+    const enlargePicture = () => {
+        // Enlarge picture
+
+    };
+    // Image from context, after it is munchified
+    const capturedImage = useContext(CapturedImageContext);
     return (
         <div>
             <div className="container mt-5">
                 <div className="row">
                     <div className="col-sm">
                         {/* Result 1 */}
-                        
-                    </div>
-                    <div className="col-sm">
-                        {/* Result 2 */}
-                        
-                    </div>
-                    <div className="col-sm">
-                        {/* Result 3 */}
+                        {/* {capturedImage?.capturedImage && (
+								<img src={capturedImage.capturedImage} alt="Munchified Image" />
+						)} */}
+                        <img src={require(`../assets/images/skriket.jpg`)} alt="Skrik by Edvard Munch" />
+                        Results 1
                     </div>
                 </div>
             </div>
