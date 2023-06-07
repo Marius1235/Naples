2eebbdae12ed5acec69e12c4357afaa838d0b15b
gisp5 <bendik.gisp5@gmail.com>
2023-06-07 15:50:47 +0200

    temp fix

--- src/components/ActionPageComponent.tsx
+++ src/components/ActionPageComponent.tsx
@@ -9,8 +9,8 @@ import { faCloudArrowUp } from '@fortawesome/free-solid-svg-icons'
 const ActionPageComponent = () => {
 	// Image from context, after it is munchified
 	var image = useContext(CapturedImageContext);
-	let base64String = image!.capturedImage;
-	let base64Data = base64String!.split(",")[1];
+	// let base64String = image!.capturedImage;
+	// let base64Data = base64String!.split(",")[1];
 	
 
 	const handleClick = async () => {
@@ -26,7 +26,7 @@ const ActionPageComponent = () => {
 				"Content-Type": "application/json",
 			},
 			body: JSON.stringify({
-				"picture": base64Data,
+				"picture": "base64Data",
 			}),
 		})
 		.then((response) => response.json())
