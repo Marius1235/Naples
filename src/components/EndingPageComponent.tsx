9663933b27e56f07aa787f57bdf1f718aef7ae1e
SleepyPlebb <espe005@student.kristiania.no>
2023-06-14 11:33:12 +0200

    EndingPage
    Made a new page where the user will be sendt after sending their artpiece. Removed qr code from action page and put it into endingPage

---
+++ src/components/EndingPageComponent.tsx
@@ -0,0 +1,11 @@
+function EndingPageComponent() {
+    return (
+        <div>
+            <h1 className='text-center' id="ending-title">Thank you for using Munchifier!</h1>
+            <img id="qr-code-image" src={require('../assets/images/qrCode.png')} alt="User feedback qr code"></img>
+			<p id="qr-text">Scan the QR code to give feedback</p>   
+        </div>
+    );
+}
+
+export default EndingPageComponent;
---
+++ src/pages/EndingPage.tsx
@@ -0,0 +1,13 @@
+import EndingPageComponent from '../components/EndingPageComponent';
+import '../css/EndingPage.css'
+
+const EndingPage = () => {
+
+    return(
+        <div className="">
+            <EndingPageComponent/>
+        </div>
+    )
+}
+
+export default EndingPage;
