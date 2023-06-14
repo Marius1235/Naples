34f2d9d1018eea216bc7d2891fc3fcd569c4839d
SleepyPlebb <espe005@student.kristiania.no>
2023-06-14 12:05:52 +0200

    update endingPage.css
    centered and made sure all the elements of the page were scaleable

--- src/components/EndingPageComponent.tsx
+++ src/components/EndingPageComponent.tsx
@@ -1,6 +1,6 @@
 function EndingPageComponent() {
     return (
-        <div>
+        <div id="parent-div">
             <h1 className='text-center' id="ending-title">Thank you for using The Munchifier!</h1>
             <img id="qr-code-image" src={require('../assets/images/qrCode.png')} alt="User feedback qr code"></img>
 			<p id="qr-text">Scan the QR code to give feedback</p>   
