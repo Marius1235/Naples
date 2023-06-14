917b7dee3808db270584911e4fc8e115268ac0e7
TrondAr <trondarne.staurset@gmail.com>
2023-06-14 14:20:05 +0200

    navbar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,16 +2,8 @@ import "../../css/HeaderComponent.css";
 
 const HeaderComponent = () => {
     return(
-        <div>
-            {/* <ul className="HeaderNav">
-                <li className="HeaderNavItem"> */}
-                <div className="container-fluid">
-                    <div className="row">
-                        <a href="/" className="Logo">Munch</a>
-                    </div>
-                </div>
-                {/* </li>
-            </ul> */}
+        <div> 
+            <a href="/" className="Logo">Munch</a>
         </div>
     );
 };
